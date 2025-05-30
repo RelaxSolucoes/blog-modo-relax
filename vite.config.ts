5127dc968064e3f560304447ad05e91e34b6b7e8
RonyMelo51 <rony.campinas@hotmail.com>
2025-05-29 23:27:17 -0300

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 ESSENCIAL para deploy estático funcionar no Vercel
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
